714bea57cdd237445a374cf016687c26d5d68fd0
ThomasCode92 <thomascode92@hotmail.com>
2023-02-04 00:01:48 +0100

    Adding a "Meals" Component

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import DUMMY_MEALS from '../../dummy-meals.json';
+
+import classes from './AvailableMeals.module.css';
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+
+  return (
+    <section className={classes.meals}>
+      <ul>{mealsList}</ul>
+    </section>
+  );
+};
+
+export default AvailableMeals;
