b25ba71561f319bf3d3559264e4c756c16565817
ThomasCode92 <thomascode92@hotmail.com>
2023-02-10 13:42:19 +0100

    Fetching Meals via Http

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-import DUMMY_MEALS from '../../dummy-meals.json';
+const FIREBASE_URL = process.env.REACT_APP_FIREBASE_URL;
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(FIREBASE_URL);
+      const responseData = await response.json();
+
+      console.log(responseData);
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        const loadedMeal = responseData[key];
+        loadedMeals.push({
+          ...loadedMeal,
+          id: key,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map(meal => (
     <MealItem
       key={meal.id}
       id={meal.id}
