b8cd41fed75644d968e082d9763f745a81808955
ThomasCode92 <thomascode92@hotmail.com>
2023-02-10 14:22:38 +0100

    Manage Meals State via useReducer()

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
@@ -7,10 +7,26 @@ import Modal from '../UI/Modal';
 
 const FIREBASE_URL = process.env.REACT_APP_FIREBASE_URL;
 
+const INITIAL_STATE = { meals: [], isLoading: true, httpError: false };
+
+const mealsReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_MEALS':
+      return { meals: action.payload, isLoading: false, httpError: false };
+    case 'SET_HTTP_ERROR':
+      return { meals: [], isLoading: false, httpError: action.payload };
+    case 'CONFIRM_HTTP_ERROR':
+      return { meals: [], isLoading: false, httpError: false };
+    default:
+      return INITIAL_STATE;
+  }
+};
+
 const AvailableMeals = () => {
-  const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [httpError, setHttpError] = useState(null);
+  const [mealsState, dispatchMealsState] = useReducer(
+    mealsReducer,
+    INITIAL_STATE
+  );
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -31,17 +47,15 @@ const AvailableMeals = () => {
         });
       }
 
-      setMeals(loadedMeals);
-      setIsLoading(false);
+      dispatchMealsState({ type: 'SET_MEALS', payload: loadedMeals });
     };
 
     fetchMeals().catch(error => {
-      setIsLoading(false);
-      setHttpError(error.message);
+      dispatchMealsState({ type: 'SET_HTTP_ERROR', payload: error.message });
     });
   }, []);
 
-  if (isLoading) {
+  if (mealsState.isLoading) {
     return (
       <section className={classes['meals-loading']}>
         <p>Loading...</p>
@@ -49,21 +63,21 @@ const AvailableMeals = () => {
     );
   }
 
-  if (httpError) {
+  if (mealsState.httpError) {
     const confirmError = () => {
-      setHttpError(false);
+      dispatchMealsState({ type: 'CONFIRM_HTTP_ERROR' });
     };
 
     return (
       <Modal onCloseModal={confirmError}>
         <div className={classes['error-message']}>
-          <p>{httpError}</p>
+          <p>{mealsState.httpError}</p>
         </div>
       </Modal>
     );
   }
 
-  const mealsList = meals.map(meal => (
+  const mealsList = mealsState.meals.map(meal => (
     <MealItem
       key={meal.id}
       id={meal.id}
