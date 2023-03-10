df0382f1a5b2c33ffa76db70512c0aa4923359d2
ThomasCode92 <thomascode92@hotmail.com>
2023-03-10 17:47:08 +0100

    Dealing With Form Submission & Getting User Input Values

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Modal from '../UI/Modal';
 import classes from './Login.module.css';
 
 const Login = props => {
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+    console.log(enteredName);
+
+    setEnteredName('');
+  };
+
   return (
     <Modal onCloseModal={props.onHideLogin}>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={formSubmissionHandler}>
         <div className={classes['form-control']}>
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
+          <input
+            type="text"
+            id="username"
+            value={enteredName}
+            onChange={nameInputChangeHandler}
+          />
         </div>
         <div className={classes['form-actions']}>
           <button className={classes.submit}>Submit</button>
